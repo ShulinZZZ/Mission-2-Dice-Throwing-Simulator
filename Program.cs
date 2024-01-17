72b74ac34699988161a85f6ea4d1610daf81fc84
Shulin Zhang <zhangshulin@go.byuh.edu>
2024-01-17 12:55:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+    }
+}
