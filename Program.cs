8e85a57071af6b3d9948e785c0542f23691b8c61
Shulin Zhang <zhangshulin@go.byuh.edu>
2024-01-17 20:57:09 -0700

    Completed version

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿internal class Program
+﻿using Mission_2_Dice_Throwing_Simulator;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        //variables
+        int numRoll = 0;
+        int[] aiOutput = [];
+
+        //object
+        Simulator diceRoller = new Simulator();
 
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
         System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        //user input 
+        numRoll = int.Parse(System.Console.ReadLine());
+        aiOutput = diceRoller.RollDice(numRoll);
+
+        //DICE ROLLING SIMULATION RESULTS
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1 % of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = "+ numRoll + ".");
+
+        //display the result
+        for (int i = 2; i < 13; i ++)
+        {
+            Console.WriteLine(i + ": ");
+            int percentage = aiOutput[i - 2] * 100 /numRoll;
+            Console.WriteLine(new String('*', percentage));
+        }
+
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye");
 
     }
 }
---
+++ Simulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2_Dice_Throwing_Simulator
+{
+    internal class Simulator
+    {
+        //generate random numbers for the dice
+        private Random Random = new Random();
+        int[] countResults = new int[11];
+
+        public Simulator() 
+        {
+        }
+        public int[] RollDice(int numRoll)
+        {
+            for(int i = 0; i< numRoll; i++)
+            {
+                int diceOne = Random.Next(1, 7);
+                int diceTwo = Random.Next(1, 7);
+                int sumResult = diceOne + diceTwo;
+                countResults[sumResult-2]++;
+
+            }
+            return countResults;
+        }
+    }
+}
